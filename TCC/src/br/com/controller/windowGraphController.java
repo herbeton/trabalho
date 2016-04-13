4e085a41e580249d459c2a27bf638029c00237ea
herbeton <herbeton7@gmail.com>
2016-04-13 18:40:57 -0300

    Adicionado os parametros setados pelo usuário no gráfico

--- TCC/src/br/com/controller/windowGraphController.java
+++ TCC/src/br/com/controller/windowGraphController.java
@@ -1,8 +1,6 @@
 package br.com.controller;
 
 import br.com.model.ConexaoDB;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.chart.Axis;
@@ -11,7 +9,6 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-
 import javax.swing.*;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -25,20 +22,24 @@ public class windowGraphController {
     public TextField txtPressaoMaxima;
     public TextField txtPressaoSetPSV;
     public Button btnPlotGraph;
+    public Double pegaMaiorTempo;
 
     @FXML LineChart<Number, Number> graphWindow;
 
-    ObservableList<XYChart.Series<Double, Double>> lineChartData = FXCollections.observableArrayList();
     @FXML
     private Axis xAxis= new NumberAxis();
     @FXML
     final Axis yAxis = new NumberAxis();
     //definindo a série
     XYChart.Series series = new XYChart.Series();
+    XYChart.Series seriesSetPressaoPSV = new XYChart.Series();
+    XYChart.Series seriesMaxPressaoPSV = new XYChart.Series();
+    XYChart.Series seriesMinPressaoPSV = new XYChart.Series();
 
 
 
     public void checkDatasPSVPlot(ActionEvent event) {
+        plotParametrosPSVUsuario();
         System.out.println("Valor: " + txtPressaoSetPSV.getText().toString());
         if(!txtPressaoSetPSV.getText().isEmpty() && !txtPressaoMaxima.getText().isEmpty()
                 && !txtPressaoMinima.getText().isEmpty()) {
@@ -64,7 +65,10 @@ public class windowGraphController {
         //creating the chart
         series.setName("Pontos da PSV sem interpolação!");
         pegandoOsDados();
-        graphWindow.getData().add(series);
+        //graphWindow.setLegendVisible(false);
+        graphWindow.getData().addAll(series, seriesSetPressaoPSV, seriesMaxPressaoPSV, seriesMinPressaoPSV);
+
+
         //graphWindow.setData(lineChartData);
         graphWindow.createSymbolsProperty();
 
@@ -72,17 +76,43 @@ public class windowGraphController {
 
     public void pegandoOsDados(){
         Connection conexao;
+        pegaMaiorTempo = 0d;
+        series.getData().clear();
+        seriesSetPressaoPSV.getData().clear();
+        seriesMaxPressaoPSV.getData().clear();
+        seriesMinPressaoPSV.getData().clear();
         try {
             conexao = ConexaoDB.conectar();
             String sql = "select * from DadosPSVGrafico";
             ResultSet resultSet = conexao.createStatement().executeQuery(sql);
             //adicionando dados a serie
             while (resultSet.next()){
-                series.getData().add(new XYChart.Data(resultSet.getDouble(3), resultSet.getDouble(2)));
+                series.getData().add(new XYChart.Data(resultSet.getDouble(3), resultSet.getDouble(2)));//tempoXpressao
+                if(resultSet.getDouble(3) > pegaMaiorTempo){
+                    pegaMaiorTempo = resultSet.getDouble(3);
+                }
             }
+            plotParameterSetMaxMinPSV();
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
 
+    public void plotParameterSetMaxMinPSV(){
+        pegaMaiorTempo = pegaMaiorTempo + 5;
+        //set PSV
+        seriesSetPressaoPSV.getData().add(new XYChart.Data(0, Double.valueOf(txtPressaoSetPSV.getText().toString())));//tempoXpressao
+        seriesSetPressaoPSV.getData().add(new XYChart.Data(pegaMaiorTempo, Double.valueOf(txtPressaoSetPSV.getText().toString())));
+        seriesSetPressaoPSV.setName("Set PSV");
+
+        //Max PSV
+        seriesMaxPressaoPSV.getData().add(new XYChart.Data(0, Double.valueOf(txtPressaoMaxima.getText().toString())));
+        seriesMaxPressaoPSV.getData().add(new XYChart.Data(pegaMaiorTempo, Double.valueOf(txtPressaoMaxima.getText().toString())));
+        seriesMaxPressaoPSV.setName("Max PSV");
+
+        //set PSV
+        seriesMinPressaoPSV.getData().add(new XYChart.Data(0, Double.valueOf(txtPressaoMinima.getText().toString())));
+        seriesMinPressaoPSV.getData().add(new XYChart.Data(pegaMaiorTempo, Double.valueOf(txtPressaoMinima.getText().toString())));
+        seriesMinPressaoPSV.setName("Min PSV");
     }
 }
